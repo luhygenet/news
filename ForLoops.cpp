6d3481d36f40c7f2aa99918f324eee26b3e62bec
luhygenet <luhygenet@gmail.com>
2024-05-22 15:53:45 +0300

    changes

---
+++ ForLoops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//	for (int i = 0; i < 10; i ++){  //iterates from 0 through 9 
+//	cout <<i << " ";   // outputs each number
+//	cout << endl;	
+//     }
+    
+//    for (int i = 0; i < 5; i ++){   //iterates from 1 through 5 
+//	cout << "Hello" << endl;   // outputs Hello
+//	cout << "*" << endl;	//outputs * after each hello
+//     }
+//     
+//    for (int i = 1; i < 20; i =  i + 2){   //iterates from 1 through 20
+//	 
+//	cout << "i: " << i << endl; // outputs first 10 odd numbers 	
+//     }
+     
+    for (int i = 10; i >= 1; i =  i - 1){   //iterates backward from 10 through 1
+	 
+	cout << "i: " << i << endl; // outputs first 10 odd numbers 	
+     }
+return 0;
+}
+
+
